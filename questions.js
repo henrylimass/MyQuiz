878620efc6aa440646dea97b76e5073a91ea589d
HENRY ARAUJO <112120971+henrylimass@users.noreply.github.com>
2024-05-03 13:12:25 -0400

    finalizado

--- questions.js
+++ questions.js
@@ -1,28 +1,46 @@
 export default[
 {
-    question: "TESTE",
+    question: "1) Qual é o objrtivo principal do inglês intrumental?",
     answers:[
-        { option: "3", correct: true},
-        { option: "3", correct: false},
-        { option: "3", correct: false},
+        { option: "Desenvolver habilidades de conversação em inglês", correct: false},
+        { option: "Aprender a gramática inglesa de forma avançada", correct: false},
+        { option: "Capacitar os alunos a compreender texto em inglês", correct: true},
     ]
 },
 
 {
-    question: "TESTE2",
+    question: "2) Qual das afirmações está correta sobre inclusão digital no ensino funtamental?",
     answers:[
-        { option: "3", correct: true},
-        { option: "3", correct: false},
-        { option: "3", correct: false},
+        { option: "Uso de computadores por professores", correct: false},
+        { option: "jogar videogame após as aulas", correct: false},
+        { option: "garantir acesso e habilidades em tecnologia para aprendizado e comunicação", correct: true},
     ]
 },
 
 {
-    question: "TESTE3",
-    answers:[
-        { option: "3", correct: true},
-        { option: "3", correct: false},
-        { option: "3", correct: false},
+    question: "3) Qual é a medida de dispersão que representa a diferença entre o maior e o menor valor em um conjunto de dados?",
+    answer: [
+        {option:"média",correct:false},
+        {option:"amplitude",correct:true},
+        {option:"mediana",correct:false},
+    ]
+},
+
+{
+    question:"4) Qual é o tipo de gráfico usado para representar a relação entre duas variáveis numéricas?",
+    answer: [
+        {option:"histograma",correct:false},
+        {option:"gráfico de pizza",correct:false},
+        {option:"gráfico de dispersão",correct:true},
+    ]
+},
+
+{
+    question:"5) Qual das seguintes afirmações está correta sobre inclusão digital no nivel de ensino fundamental?",
+    answer: [
+        {option:"ensino exclusivo de programação de computadores",correct:false},
+        {option:"uso de dispositivos digitais apenas durante as aulas de educação física",correct:false},
+        {option:"garantir que todos os alunos tenham acesso a tecnologias para aprendizado e comunicação",correct:true},
     ]
 },
 ]
