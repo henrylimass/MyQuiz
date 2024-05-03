fb49c420f2cf437d63f915b3c74892d55953bb64
HENRY ARAUJO <112120971+henrylimass@users.noreply.github.com>
2024-05-03 13:01:04 -0400

    codigo finalizado

--- questions.js
+++ questions.js
@@ -8,6 +8,21 @@ export default[
     ]
 },
 
+{
+    question: "TESTE2",
+    answers:[
+        { option: "3", correct: true},
+        { option: "3", correct: false},
+        { option: "3", correct: false},
+    ]
+},
 
-
+{
+    question: "TESTE3",
+    answers:[
+        { option: "3", correct: true},
+        { option: "3", correct: false},
+        { option: "3", correct: false},
+    ]
+},
 ]
